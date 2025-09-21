541276c1c4ab40b39eb9633ef8928f04197a3986
jvilladolid <116508041+jobVilladolid@users.noreply.github.com>
2025-09-21 19:21:10 +1000

    -Changed map to ESRI outdoor

--- src/main/java/com/example/globetrotter/view/OverrideTileRetriever.java
+++ src/main/java/com/example/globetrotter/view/OverrideTileRetriever.java
@@ -11,8 +11,10 @@ public class OverrideTileRetriever implements TileRetriever {
         System.out.println("TracesTrackTileRetriever constructor called!");
     }
 
-    private static final String HOST = "https://api.maptiler.com/tiles/satellite-mediumres-2018/";
-    private static final String API_KEY = "AqHyscnhDxmUUlar0GzY";
+    // https://basemaps-api.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer/tile/1/0/0.pbf?token=
+
+    private static final String HOST = "https://static-map-tiles-api.arcgis.com/arcgis/rest/services/static-basemap-tiles-service/v1/arcgis/outdoor/static/tile/";
+    private static final String API_KEY = "AAPTxy8BH1VEsoebNVZXo8HurLGGQvr8xBwq6kPfaUltn5Urn8soXY7wCLc6ItJlSkpO2jjWWTpz_ASP5gh3d8UVYeEiEiLAO5bfczYpWOgNAGAPvGUMihYXc7lXi31xsF7bisJpnqQPJxNMDaL39mgBH3sjZ7H_GtdQBX-QoI2R-fva9mcg12eEb7GpAXAMtgm9GANyLvfMGLUIVRMvL6aSMwcm9iewwu7cS8jR2eX2PUM.AT1_uYmxm5nK";
 
     static final String httpAgent;
 
@@ -20,8 +22,8 @@ public class OverrideTileRetriever implements TileRetriever {
     static String buildImageUrlString(int zoom, long x, long y) {
         long numTiles = 1L << zoom; // 2^zoom
         long wrappedX = ((x % numTiles) + numTiles) % numTiles; // wrap x
-        String url = HOST + zoom + "/" + wrappedX + "/" + y + ".png?key=" + API_KEY;
-        System.out.println("Loading tile: " + url);
+        String url = HOST + zoom + "/" + y + "/" + wrappedX + "?token=" + API_KEY;
+        // System.out.println("Loading tile: " + url);
         return url;
     }
 

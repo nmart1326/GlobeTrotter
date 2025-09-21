0c41f4eade4f78d0b069d7577380f6caec263a1d
Noyal Martin <n12107948@qut.edu.au>
2025-09-21 19:31:13 +1000

    Changed map to children's map

--- src/main/java/com/example/globetrotter/main/globeTrotterApp.java
+++ src/main/java/com/example/globetrotter/main/globeTrotterApp.java
@@ -3,6 +3,7 @@ package com.example.globetrotter.main;
 import com.example.globetrotter.controller.MainController;
 import com.example.globetrotter.controller.QuizController;
 import com.example.globetrotter.service.QuizService;
+import com.example.globetrotter.view.LoginView;
 import com.example.globetrotter.view.Map;
 import com.example.globetrotter.view.QuizView;
 import com.example.globetrotter.view.SidePanel;
@@ -25,12 +26,13 @@ public class globeTrotterApp extends Application {
     private Button toggleButton;
     private boolean isSidePanelHidden = false;
     private final int SPWidth = 400;
+    private LoginView loginView;
 
     @Override
     public void start(Stage primaryStage) {
         // Loads icon on the title bar of the window, and taskbar
         try{
-            Image icon = new Image(getClass().getResourceAsStream("/assets.icons/globetrotter.png"));
+            Image icon = new Image(getClass().getResourceAsStream("/assets/icons/globetrotter.png"));
             primaryStage.getIcons().add(icon);
         } catch (Exception e){
             System.err.println("Failed to load application icon: " + e.getMessage());
@@ -40,13 +42,15 @@ public class globeTrotterApp extends Application {
         mapView = new Map();
         sidePanel = new SidePanel();
         toggleButton = ToggleButtonFactory.createButton();
+        loginView = new LoginView();
+
         // Initialize the version label
         Label versionLabel = new Label("Globetrotter v0.1.0");
 
 
         // UI Layers from Bottom->Top: {versionLabel✓, map✓, QuizTablet, NavigationsidePanel✓, LoginScreen}
         StackPane root = new StackPane();
-        root.getChildren().addAll(versionLabel, mapView.getMapView(), sidePanel.getSidePanel(), toggleButton);
+        root.getChildren().addAll(versionLabel, mapView.getMapView(), sidePanel.getSidePanel(), toggleButton, loginView);
 
         StackPane.setAlignment(sidePanel.getSidePanel(), Pos.CENTER_LEFT);
         StackPane.setAlignment(toggleButton, Pos.TOP_LEFT);
@@ -57,7 +61,7 @@ public class globeTrotterApp extends Application {
         new MainController(sidePanel, toggleButton, "home.png");
 
 
-        Scene scene = new Scene(root, 1500, 750);
+        Scene scene = new Scene(root, 1280, 720);
         //Load Fonts
         scene.getStylesheets().add(getClass().getResource("/com/example/globetrotter/styles.css").toExternalForm());
 
